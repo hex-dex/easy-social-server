ec4ec76636cb13052bfbbd61a043e333188bbadb
fzippy <hk2011khan@gmail.com>
2022-11-05 23:40:07 +0000

    improved login system

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var apiRouter = require('./api/index');
-
-var app = express();
+const createError = require("http-errors");
+const express = require("express");
+const apiRouter = require("./api/index");
+require("dotenv").config();
+const mysql = require("mysql2");
+const conn = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const bcrypt = require("bcrypt");
+const app = express();
 const PORT = 4000;
+const saltRounds = 12;
 
-app.use('/api', apiRouter);
+const userExists = async (username) => {
+  const result = await conn
+    .promise()
+    .query(`SELECT * from users WHERE username = '${username}' `)
+    .then(([rows, fields]) => {
+      if (rows.length > 0) {
+        return true;
+      }
+      return false;
+    });
+  return result;
+};
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+const addUser = (username, password, email) => {
+  let noErr = true;
+  bcrypt.hash(password, saltRounds, (err, data) => {
+    conn.query(
+      `INSERT INTO users (username,password,email,date_joined) VALUES ('${username}','${data}','${email}', '${new Date()}')`,
+      (err, result) => {
+        if (err) noErr = false;
+      }
+    );
+  });
+  return noErr;
+};
+
+app.post("/register", urlencodedParser, async (req, res) => {
+  const { username, password } = req.body;
+  const test = "Ben13";
+  if ((await userExists(test)) == true) {
+    res.status(409).send({ status: 409, message: "User already exists" });
+  }
+  addUser("Jamie", "asasdasd", "YoolaBoy123");
+  // conn.query("INSERT stuff in stuff", (err, result) => {
+  //   if (!err) res.send({ status: 201, message: "Successfully logged in!" });
+  // });
+
+  res.send("hello");
+  // const hashedPassword =
+  // console.log(data[0]);
 });
-
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
 
 app.listen(PORT);
