37c92d57a007e7c5bfd8819594e4a02137185372
fzippy <hk2011khan@gmail.com>
2022-11-12 21:19:56 +0000

    removed unnecessary bug

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 // THIS IS THE SOURCE OF ALL CONTROLLERS (CONTROLLERS ARE FUNCTIONS THAT GET HIT ON AN ENDPORT) IF YOU DONT KNOW WHAT ENDPOINTS ARE THEN FUCK YOU
 function routes(app, { urlencodedParser }) {
-  "yur.com/user";
   app.use("/user", require("./user"));
   app.use("/post", require("./post"));
 }
