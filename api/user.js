954264f4dfaf7aca860e4cc4e4f1d4703ac2ed53
fzippy <hk2011khan@gmail.com>
2022-11-12 20:19:56 +0000

    fixed a bug

--- api/user.js
+++ api/user.js
@@ -6,16 +6,18 @@ const { userExists, isUserAuth } = require("../model/userModel");
 const test = "jamie";
 /* GET users listing. */
 router.post("/register", urlencodedParser, async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, email } = req.body;
   if ((await userExists("")) == true) {
     res.status(409).send({ status: 409, message: "User already exists" });
   }
-  addUser("Jamie", "asasdasd", "YoolaBoy123");
-  res.send("hello");
+  addUser(username, password, email);
+  res.status(201).send({ status: 201, message: "User successfully created" });
 });
 
 router.post("/login", urlencodedParser, async (req, res) => {
-  if ((await isUserAuth(test, "asasdasd")) == true) {
+  const { username, password } = req.body;
+
+  if ((await isUserAuth(username, password)) == true) {
     const token = await createAccessToken(test);
 
     res.status(200).send({
