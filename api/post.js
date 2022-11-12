7ab32106b963c36af044a120af33b4a5ff061027
fzippy <hk2011khan@gmail.com>
2022-11-12 21:02:04 +0000

    added post function

--- api/post.js
+++ api/post.js
@@ -1,2 +1,8 @@
-// /post/ GET
-router.get("/", (req, res) => {});
+const express = require("express");
+const router = express.Router();
+const { addPost } = require("../model/postModel");
+router.post("/add-post", (req, res) => {
+  addPost();
+  res.status(201).send({ status: 201, message: "Post successfully created" });
+});
+module.exports = router;
--- model/postModel.js
+++ model/postModel.js
@@ -1,1 +1,24 @@
-const addPost = () => {};
+const { Client } = require("cassandra-driver");
+const { v4: uuidv4 } = require("uuid");
+
+require("dotenv").config();
+const client = new Client({
+  cloud: {
+    secureConnectBundle: "./secure-connect-posts.zip",
+  },
+  credentials: {
+    username: `${process.env.clientID}`,
+    password: `${process.env.clientSecret}`,
+  },
+});
+
+// Execute a query
+
+// Run the async function
+exports.addPost = async (username, content) => {
+  UUID = uuidv4();
+  const addItem = await client.execute(
+    `INSERT INTO useractivities.posts (post_ID, source_ID, post_content, date_created) VALUES ('${UUID}', '${username}', '${content}', '${new Date()}');`
+  );
+  addItem;
+};
