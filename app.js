b537db89cfead38cbe7ecbeaac471971900e8022
CrampedAverage <adnan.aant@gmail.com>
2022-11-13 01:03:21 +0000

    enabled cors

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const createError = require("http-errors");
 const express = require("express");
 const apiRouter = require("./api/index");
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+const corsOptions = {
+  origin: "http://127.0.0.1:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 const app = express();
 const PORT = 4000;
-const saltRounds = 12;
 const routes = require("./api");
 
+app.use(cors(corsOptions));
 routes(app, { urlencodedParser });
 
 app.listen(PORT);
