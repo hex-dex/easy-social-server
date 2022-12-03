a47b45373279b5d0446ccd30b36441d708502f7e
fzippy <hk2011khan@gmail.com>
2022-12-03 22:40:34 +0000

    minor Changes

--- api/post.js
+++ api/post.js
@@ -8,12 +8,12 @@ const { createAccessToken, verifyToken } = require("../service/tokenServices");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 router.use(bodyParser.json());
 
-router.post("/", (req, res) => {
+router.post("/create-post", (req, res) => {
   const { cookieValue, post_content } = req.body;
   addPost(verifyToken(cookieValue), post_content);
   res.status(201).send({ status: 201, message: "Post successfully created" });
 });
-router.get("/", async (req, res) => {
+router.get("/get-all-post", async (req, res) => {
   results = await getAllPost();
 
   res.status(200).send({ status: 200, data: results });
