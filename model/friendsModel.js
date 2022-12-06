79bf2d5d8853f0dad0696764e5bc300b9ced208e
fzippy <hk2011khan@gmail.com>
2022-12-06 22:30:43 +0000

    Added friends functions

--- model/friendsModel.js
+++ model/friendsModel.js
@@ -1,13 +1,59 @@
 const mysql = require("mysql2");
 const conn = mysql.createConnection(process.env.DATABASE_URL);
-checkFriendUnique = async function (username, target) {
+const { v4: uuidv4 } = require("uuid");
+
+isFriendUnique = async function (username, target) {
   res = await conn
     .promise()
     .query(
-      `SELECT friendship_unique WHERE src_user = ${username} AND target_user = ${target} OR src_user = ${target} AND target_user = ${username} `
+      `SELECT friendship_unique from friends WHERE src_user = '${username}' AND target_user = '${target}' OR src_user = '${target}' AND target_user = '${username}' `
     )
-    .then((rows, fields) => {});
+    .then(([rows, fields]) => {
+      if (rows.length >= 1) {
+        return rows[0].friendship_unique;
+      } else {
+        return false;
+      }
+    });
+  return res;
 };
-requestFriend = function (username) {
-  console.log();
+exports.requestFriend = async function (username, srcID) {
+  let UUID = uuidv4();
+  let date = new Date();
+  if ((await isFriendUnique(username, srcID)) == false) {
+    const res = await conn
+      .promise()
+      .query(
+        `INSERT INTO friends (src_user,target_user,status,friendship_unique,created_at,updated_at) VALUES( '${username}', '${srcID}', 'PENDING', '${UUID}', '${date}','${date}')`
+      );
+  } else {
+    let friendID = await isFriendUnique(username, srcID);
+    const res = await conn
+      .promise()
+      .query(
+        `INSERT INTO friends (src_user,target_user,status,friendship_unique,created_at,updated_at) VALUES( '${username}', '${srcID}', 'PENDING', '${friendID}', '${date}','${date}')`
+      );
+  }
+};
+exports.showPendingFriends = async function (username) {
+  const res = await conn
+    .promise()
+    .query(
+      `SELECT * from friends WHERE target_user = '${username}' AND status = 'PENDING' `
+    )
+    .then(([rows, fields]) => {
+      return rows;
+    });
+  return res;
+};
+exports.showFriends = async function (username) {
+  const res = await conn
+    .promise()
+    .query(
+      `SELECT * from friends WHERE target_user = '${username}' AND status = 'ACTIVE' `
+    )
+    .then(([rows, fields]) => {
+      return rows;
+    });
+  return res;
 };
