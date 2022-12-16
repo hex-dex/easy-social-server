09d15f641edeb6dbddcb1af5ed26a3ce3a084613
fzippy <hk2011khan@gmail.com>
2022-12-16 21:18:53 +0000

    created recieve msg endpoint

--- api/message.js
+++ api/message.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 const { verifyToken } = require("../service/tokenServices");
-const { createPost } = require("../model/messagesModal");
+const { createMessage, getChatMessages } = require("../model/messagesModal");
 const { getFriendUnique } = require("../model/friendsModel");
 router.use(bodyParser.json());
 
@@ -11,7 +11,12 @@ router.post("/send-message", async (req, res) => {
   const { username, target_user } = req.body;
   let chatID = await getFriendUnique(username, target_user);
   let msgBody = "Hello my firstMSG";
-  createPost(username, target_user, msgBody, chatID);
+  createMessage(username, target_user, msgBody, chatID);
   res.send("<h1>Y</h>");
+});
+router.get("/get-messages", async (req, res) => {
+  let chatID = "40392600-3555-4308-82f3-0b1200b7b8a3";
+  let resulkt = await getChatMessages(chatID);
+  res.send(resulkt);
 });
 module.exports = router;
