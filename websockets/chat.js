0be1a861a263b31be1f6435a64e3033369a57cb9
fzippy <hk2011khan@gmail.com>
2022-12-19 05:32:49 +0000

    Websocket function

--- websockets/chat.js
+++ websockets/chat.js
@@ -1,15 +1,25 @@
-const { WebSocketServer } = require("ws");
+const { WebSocketServer, WebSocket } = require("ws");
+const { v4: uuidv4 } = require("uuid");
 
 const chat = new WebSocketServer({ noServer: true });
+const rooms = {};
 
-chat.on("connection", (ws) => {
-  ws.on("message", (data) => {
-    console.log("message", JSON.parse(data.toString()));
+chat.on("connection", (socket) => {
+  socket.id = uuidv4();
+
+  socket.on("message", function (data) {
+    const { message, room } = JSON.parse(data);
+    // const rooms = {12321: {unique: ws}}
+    if (!rooms[room]) rooms[room] = {};
+    rooms[room] = { ...rooms[room], [socket.id]: socket };
+    console.log(rooms[room]); // join the room
+
+    Object.values(rooms[room]).forEach((client) => {
+      if (client != socket && client.readyState === WebSocket.OPEN) {
+        client.send(`Got ur msg` + data);
+      }
+    });
   });
-  ws.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-  ws.send("test");
 });
 
 module.exports = chat;
